ddf56c347fc007f81488edd97f82f462edb86484
Govindaraj773 <106619884+Govindaraj773@users.noreply.github.com>
2023-11-18 18:18:35 +0530

    Adding lists
    Adding lists and objects problem.

---
+++ lists/script.js
@@ -0,0 +1,16 @@
+var obj = {name:"Basu", place:"Bangalore"};
+var obj2 = obj
+console.log(obj2)
+
+obj.name = "Govi"
+console.log(obj2);
+
+
+
+// Example 1
+let p = 10;
+if(p===10){
+    let p = 20;
+    console.log(p)
+}
+console.log(p)
